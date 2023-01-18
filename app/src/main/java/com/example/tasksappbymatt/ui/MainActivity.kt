fef902889d27c384e3f4f9f9460ec7da1f5f64e7
Matvey Dranitsyn <92252237+DMatvei@users.noreply.github.com>
2023-01-18 22:04:40 +1000

    moved file

--- app/src/main/java/com/example/tasksappbymatt/ui/MainActivity.kt
+++ app/src/main/java/com/example/tasksappbymatt/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.tasksappbymatt
+package com.example.tasksappbymatt.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.tasksappbymatt.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
