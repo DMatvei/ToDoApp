f5a74d7feaa0665084a2ecaea57a6f5acc932a66
Matvey Dranitsyn <92252237+DMatvei@users.noreply.github.com>
2023-01-18 22:03:16 +1000

    Create display fragment in Navigation Graph

---
+++ app/src/main/java/com/example/tasksappbymatt/ui/tasks/TasksFragment.kt
@@ -0,0 +1,7 @@
+package com.example.tasksappbymatt.ui.tasks
+
+import androidx.fragment.app.Fragment
+import com.example.tasksappbymatt.R
+
+class TasksFragment : Fragment(R.layout.fragment_tasks) {
+}
