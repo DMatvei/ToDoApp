1c3532f554f5c8b9e0d93c443598ea24f45cdbeb
Matvey Dranitsyn <92252237+DMatvei@users.noreply.github.com>
2023-01-20 23:49:45 +1000

    add delete all completed tasks functional

---
+++ app/src/main/java/com/example/tasksappbymatt/ui/deleteallcompleted/DeleteAllCompletedDialogFragment.kt
@@ -0,0 +1,26 @@
+package com.example.tasksappbymatt.ui.deleteallcompleted
+
+import android.app.Dialog
+import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.viewModels
+import dagger.hilt.android.AndroidEntryPoint
+
+
+@AndroidEntryPoint
+class DeleteAllCompletedDialogFragment : DialogFragment() {
+
+    private val viewModel: DeleteAllCompletedViewModel by viewModels()
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog =
+        AlertDialog.Builder(requireContext())
+            .setTitle("Подтверждение удаления")
+            .setMessage("Вы действительно хотите удалить все выполненые задачи?")
+            .setNegativeButton("Отмена", null)
+            .setPositiveButton("Конечно") { _, _ ->
+                viewModel.onConfirmClick()
+            }
+            .create()
+
+}
---
+++ app/src/main/java/com/example/tasksappbymatt/ui/deleteallcompleted/DeleteAllCompletedViewModel.kt
@@ -0,0 +1,21 @@
+package com.example.tasksappbymatt.ui.deleteallcompleted
+
+import androidx.lifecycle.ViewModel
+import com.example.tasksappbymatt.data.TaskDao
+import com.example.tasksappbymatt.di.ApplicationScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+
+@HiltViewModel
+class DeleteAllCompletedViewModel @Inject constructor(
+    private val taskDao: TaskDao,
+    @ApplicationScope private val applicationScope : CoroutineScope
+): ViewModel(){
+
+    fun onConfirmClick() = applicationScope.launch {
+        taskDao.deleteCompletedTasks()
+    }
+}
